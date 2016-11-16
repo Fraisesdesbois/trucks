35ac0116d26fa4d90b4ff7baefb5908d33fa3c21
Mael Machane <mach.mehd@gmail.com>
2016-11-16 22:26:34 +0100

    controller machine ok

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,30 +1,24 @@
 class MachinesController < ApplicationController
+  before_action :set_machine, only: [:show, :edit, :update, :destroy]
+
   def index
     @machines = Machine.all
   end
   def show
-    @machine = Machine.find(params[:id])
   end
   def new
     @machine = Machine.new
   end
   def edit
-    @machine = Machine.find(params[:id])
-
   end
   def create
     machine = Machine.create(machine_params)
     redirect_to machines_path
-
   end
   def update
-    @machine = Machine.find(params[:id])
-    @machine.update(machine_params)
     redirect_to machines_path
-
   end
   def destroy
-    @machine = Machine.find(params[:id])
     @machine.destroy
     redirect_to machines_path
   end
@@ -33,5 +27,9 @@ class MachinesController < ApplicationController
 
   def machine_params
     params.require(:machine).permit(:name, :brand)
+  end
+
+  def set_machine
+    @machine = Machine.find(params[:id])
   end
 end
